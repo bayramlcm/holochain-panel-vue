f299792cef914381cb45e650cd748e68e1f3c810
Bayram ALAÇAM <bayramlcm14@gmail.com>
2020-06-08 16:12:14 +0300

    Kullanıcı ekleme / çağırma servis testinin yapılması

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -19,13 +19,38 @@ export default {
       if (this.holochainConnected) {
         console.log({ holo: this.holochainConnection });
 
-        const ax = this.holochainConnection(
+        // Kullanıcı oluştur
+        this.holochainConnection(
           holochainConfig.INSTANCE_NAME,
           holochainConfig.ZOOM_NAME,
-          "hello_holo"
-        )({ args: {} })
-          .then(data => console.log({ data }))
+          "create_person"
+        )({ person: { name: "Bayram ALAÇAM" } })
+          .then(data => {
+            const result = JSON.parse(data);
+            const userId = result.Ok;
+            console.log({ userId });
+            // Kullanıcının verilerini al
+            this.holochainConnection(
+              holochainConfig.INSTANCE_NAME,
+              holochainConfig.ZOOM_NAME,
+              "retrieve_person"
+            )({ address: userId })
+              .then(data => {
+                const result = JSON.parse(data);
+                const name = result.Ok
+                console.log({ name });
+              })
+              .catch(err => console.log({ err }));
+          })
           .catch(err => console.log({ err }));
+        // HELLO
+        // const ax = this.holochainConnection(
+        //   holochainConfig.INSTANCE_NAME,
+        //   holochainConfig.ZOOM_NAME,
+        //   "hello_holo"
+        // )({ args: {} })
+        //   .then(data => console.log({ data }))
+        //   .catch(err => console.log({ err }));
       } else {
         console.log("Holochain bağlantısı yok!");
       }
